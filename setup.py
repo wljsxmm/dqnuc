96e0864f2763048a7b734fd1c0b31d64fc164a18
xmm <xgl21@mails.tsinghua.edu.cn>
2022-05-27 21:01:29 +0800

    1

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+pkgs = {
+    "required": [
+        "numpy",
+        "pandas",
+    ]
+}
+
+setuptools.setup(
+    name="rl4uc", 
+    version="0.1.1",
+    author="Patrick de Mars",
+    author_email="pwdemars@gmail.com",
+    description="Reinforcement learning environment for the unit commitment problem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=pkgs["required"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={'rl4uc': ['data/*.csv']},
+)
---
+++ test.py
@@ -0,0 +1,19 @@
+import numpy as np
+from rl4uc.environment import make_env_from_json
+from gym import envs
+env_specs = envs.registry.all()
+envs_ids = [env_spec.id for env_spec in env_specs]
+print(envs_ids)
+import gym
+import rl4uc.environment
+
+'''
+env = gym.make('MountainCar-v0')
+print('观测空间 = {}'.format(env.observation_space))
+print('动作空间 = {}'.format(env.action_space))
+print('观测范围 = {} ~ {}'.format(env.observation_space.low,
+        env.observation_space.high))
+print('动作数 = {}'.format(env.action_space.n))
+'''
+
+
